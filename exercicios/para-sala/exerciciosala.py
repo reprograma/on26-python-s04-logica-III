220c0b6a15b829d236ee756bf8aac90dce1b795a
Jessica <jessicacristina.recife@gmail.com>
2023-08-26 14:31:32 -0300

    exercicio sala

---
+++ exercicios/para-sala/exerciciosala.py
@@ -0,0 +1,24 @@
+#Vamos mudar nosso laço para imprimir um intervalo inciado em 0 (zero) com n
+# numeros informado pelo usuário
+
+#numero = int(input("informe o numero: "))
+#valor_atual = 0
+
+#while valor_atual <= numero:
+#    print(valor_atual)
+#    valor_atual = valor_atual + 1
+
+#print("FIM DO PROGRAMA")
+
+#########################################################################################
+### exemplo range()
+#for numero in range(1, 10, 2):  #inicio, parada, step
+#    print(numero)
+
+##########################################################################################
+
+variavel_texto = "jessica cristina"
+
+for caracter in variavel_texto:
+    print(caracter)
+print(variavel_texto)

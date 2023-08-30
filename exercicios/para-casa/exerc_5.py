6413c1926fc499dc85d1d0aeeaf4a396a50a7386
Lais Meireles Alves <lameireles2@gmail.com>
2023-08-30 19:08:15 -0300

    arquivos do exercicio da semana2

---
+++ exercicios/para-casa/exerc_5.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia 20 números inteiros e armazene-os em um vetor.
+# Armazene os números pares no vetor PAR e os números ímpares no vetor ÍMPAR.
+# Imprima os três vetores.
+
+# Função range para imprimir a sequência de 1 a 21
+print(f"Lista de vinte números inteiros: {list(range(1,21))}")
+
+# Função range para imprimir apenas os números pares contidos na sequência de 1 a 21
+print(f"PAR: {list(range(2,21,2))}")
+
+# Função range para imprimir apenas os números ímpares contidos na sequência de 1 a 21
+print(f"ÍMPAR: {list(range(1,21,2))}")

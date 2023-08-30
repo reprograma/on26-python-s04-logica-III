feac078444645012827c440acbc958f62b986644
juliabretas <juliabretasgs@gmail.com>
2023-08-30 18:32:45 -0300

    exercícios da semana4

---
+++ exercicios/para-casa/entrega/ex5jb.py
@@ -0,0 +1,11 @@
+#Faça um Programa que leia 20 números inteiros e armazene-os num vetor. 
+# Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. 
+# Imprima os três vetores.
+
+numero = 6
+
+while numero >5:
+    print(numero)
+    numero = numero +1
+    
+
